941f56fbd85c4a2ac9e4e1d30e4b2f53cc91ad21
silentcoffin <135639059+silentcoffin@users.noreply.github.com>
2024-02-25 09:22:46 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# Define common variables
+# Common variables for device and ROM
 LOCAL_PRODUCT_NAME := carbon
 LOCAL_DEVICE := gauguin
 
-# Common settings for all build variants
+# Set product characteristics for all build variants
 PRODUCT_DEVICE := $(LOCAL_DEVICE)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 10i
@@ -12,27 +12,25 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MAKEFILES := \\
     $(LOCAL_DIR)/$(LOCAL_PRODUCT_NAME)_$(LOCAL_DEVICE).mk
 
-# Include CarbonROM common configurations
+# Include common configurations for CarbonROM
 $(call inherit-product, vendor/$(LOCAL_PRODUCT_NAME)/config/common_full_phone.mk)
 
-# Build variant specific settings
+# Define build variant specific product names
 ifeq ($(TARGET_BUILD_VARIANT),userdebug)
     PRODUCT_NAME := $(LOCAL_PRODUCT_NAME)_$(LOCAL_DEVICE)-userdebug
 endif
-
 ifeq ($(TARGET_BUILD_VARIANT),user)
     PRODUCT_NAME := $(LOCAL_PRODUCT_NAME)_$(LOCAL_DEVICE)-user
 endif
-
 ifeq ($(TARGET_BUILD_VARIANT),eng)
     PRODUCT_NAME := $(LOCAL_PRODUCT_NAME)_$(LOCAL_DEVICE)-eng
 endif
 
-# Additional features and settings
+# Set additional features and overrides for the product
 PRODUCT_PROPERTY_OVERRIDES += \\
     ro.$(LOCAL_PRODUCT_NAME).version=$(CARBON_VERSION) \\
     ro.$(LOCAL_PRODUCT_NAME).buildtype=$(TARGET_BUILD_VARIANT)
 
-# Define the default system language and locale for India
+# Configure the default system language and locale for the Indian market
 PRODUCT_DEFAULT_LANGUAGE := en
 PRODUCT_DEFAULT_REGION := IN
